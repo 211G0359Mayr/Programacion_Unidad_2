ecbb7e9f9adab137d1d8829be48977ab75dab135
alber <alber@LECONA>
2024-11-08 16:58:24 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Programacion_Unidad_2.Models;
+using Programacion_Unidad_2.Models.ViewModels;
+
+
+namespace Programacion_Unidad_2.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			MapaCurricularContext context = new();
+			var Indx = context.Carreras.OrderBy(x => x.Nombre);
+			return View(Indx);
+		}
+
+
+		[Route("informacion/{Informacion}")]
+		public IActionResult Informacion(string Informacion)
+		{
+			string nom = Informacion.Replace("-", " ");
+			MapaCurricularContext context = new();
+			var Inf = context.Carreras.FirstOrDefault(x => x.Nombre == Informacion || nom == x.Nombre);
+
+			if (Inf == null)
+			{
+				RedirectToAction("Index");
+			}
+			return View(Inf);
+		}
+		[Route("Mapa/{Mapa}")]
+		public IActionResult Mapa(string Mapa)
+		{
+			string nombreCarrera = Mapa.Replace("-", " ");
+			MapaCurricularContext context = new();
+			var carrera = context.Carreras
+				.Include(x => x.Materias)
+				.FirstOrDefault(c => c.Nombre == Mapa || c.Nombre == nombreCarrera);
+
+			if (carrera == null)
+			{
+				return RedirectToAction("Index");
+			}
+
+			var viewModel = new CarreraViewModel
+			{
+				Carrera = carrera.Nombre,
+				Programa = carrera.Plan,
+				CreditosTotales = carrera.Materias.Sum(x => x.Creditos),
+				Semestres = Enumerable.Range(1, 9).Select(i => new SemestreView
+				{
+					NumSemestre = i,
+					Materias = carrera.Materias
+						.Where(m => m.Semestre == i)
+						.Select(m => new MateriaView
+						{
+							Codigo = m.Clave,
+							Titulo = m.Nombre,
+							Creditos = m.Creditos,
+							Practica = m.HorasPracticas,
+							Teoria = m.HorasTeoricas
+						})
+						.ToList()
+				}).ToList()
+			};
+
+			return View(viewModel);
+		}
+	}
+	}
---
+++ Models/ViewModels/MateriasViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Programacion_Unidad_2.Models.ViewModels
+{
+    public class CarreraViewModel
+    {
+        public string Carrera { get; set; }
+        public string Programa { get; set; }
+        public int CreditosTotales { get; set; }
+        public List<SemestreView> Semestres { get; set; }
+    }
+
+    public class SemestreView
+    {
+        public int NumSemestre { get; set; }
+        public List<MateriaView> Materias { get; set; }
+    }
+
+    public class MateriaView
+    {
+        public string Codigo { get; set; }
+        public string Titulo { get; set; }
+        public int Creditos { get; set; }
+        public int Practica { get; set; }
+        public int Teoria { get; set; }
+    }
+}
